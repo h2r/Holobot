88477d2496d9db2a318c07d9ba0d113359ab2e92
Gary Chien <garychien1@gmail.com>
2018-04-03 20:34:08 -0400

    waypoint placement working

--- Assets/Scripts/SpatialPlace.cs
+++ Assets/Scripts/SpatialPlace.cs
@@ -2,74 +2,94 @@
 using UnityEngine.UI;
 using System;
 
-public class SpatialPlace : MonoBehaviour
+namespace Academy.HoloToolkit.Unity
 {
-    bool placing = false;
-    bool firstPlace = true;
-    int waypoint_ind = 0;
-
-    // Called by GazeGestureManager when the user performs a Select gesture
-    void OnSelect()
+    public class SpatialPlace : MonoBehaviour
     {
-        // On each Select gesture, toggle whether the user is in placing mode.
-        placing = !placing;
+        bool placing = true;
+        bool waypointPlaced = false;
+        int waypointInd;
+        GameObject waypointObj;
+        GameObject coordTextObj;
+        Text coordText;
 
-        // If the user is in placing mode, display the spatial mapping mesh.
-        if (placing)
+        private void Start()
         {
             SpatialMapping.Instance.DrawVisualMeshes = true;
+            InitializeWaypoint();
         }
-        // If the user is not in placing mode, hide the spatial mapping mesh.
-        else
-        {
-            SpatialMapping.Instance.DrawVisualMeshes = false;
-        }
-        if (!placing && !firstPlace)
-        {
-            waypoint_ind++;
-            GameObject newWaypoint = Instantiate(gameObject);
-            newWaypoint.name = String.Format("Waypoint{0}", waypoint_ind);
-            // TODO: change child text name
-            Debug.Log(String.Format("New Waypoint: {0}", newWaypoint.name));
-            placing = true;
-            SpatialMapping.Instance.DrawVisualMeshes = true;
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        // If the user is in placing mode,
-        // update the placement to match the user's gaze.
-
-        if (placing)
+        void InitializeWaypoint()
         {
-            if (firstPlace)
+            waypointObj = gameObject;
+            waypointInd = WaypointManager.Instance.waypointInd;
+            waypointObj.name = String.Format("Waypoint{0}", waypointInd);
+            coordTextObj = waypointObj.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject;
+            coordTextObj.name = String.Format("WaypointCoord{0}", waypointInd);
+            WaypointManager.Instance.waypointInd++;
+        }
+
+        // Called by GazeGestureManager when the user performs a Select gesture
+        void OnSelect()
+        {
+            // On each Select gesture, toggle whether the user is in placing mode.
+            //placing = !placing;
+
+            //if (firstPlace)
+            //{
+            //    waypointObj = gameObject;
+            //    InitializeWaypoint();
+            //    Debug.Assert(coordTextObj != null);
+            //}
+            if (waypointPlaced)
             {
-                firstPlace = false;
+                waypointObj = Instantiate(gameObject);
+                placing = false;
             }
-            // Do a raycast into the world that will only hit the Spatial Mapping mesh.
-            var headPosition = Camera.main.transform.position;
-            var gazeDirection = Camera.main.transform.forward;
+        }
 
-            RaycastHit hitInfo;
-            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
-                30.0f, SpatialMapping.PhysicsRaycastMask))
+        // Update is called once per frame
+        void Update()
+        {
+            // If the user is in placing mode,
+            // update the placement to match the user's gaze.
+
+            if (placing)
             {
-                // Move this object's parent object to
-                // where the raycast hit the Spatial Mapping mesh.
-                this.transform.position = hitInfo.point;
+                //Debug.Log(String.Format("Placing Waypoint {0}", waypoint_ind));
+                
+                // Do a raycast into the world that will only hit the Spatial Mapping mesh.
+                var headPosition = Camera.main.transform.position;
+                var gazeDirection = Camera.main.transform.forward;
 
-                // Rotate this object's parent object to face the user.
-                Quaternion toQuat = Camera.main.transform.localRotation;
-                toQuat.x = 0;
-                toQuat.z = 0;
-                this.transform.rotation = toQuat;
+                RaycastHit hitInfo;
+                if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+                    30.0f, SpatialMapping.PhysicsRaycastMask))
+                {
+                    // Move this object's parent object to
+                    // where the raycast hit the Spatial Mapping mesh.
+                    //this.transform.position = hitInfo.point;
+                    waypointObj.transform.position = hitInfo.point;
+
+                    // Rotate this object's parent object to face the user.
+                    Quaternion toQuat = Camera.main.transform.localRotation;
+                    toQuat.x = 0;
+                    toQuat.z = 0;
+                    //this.transform.rotation = toQuat;
+                    waypointObj.transform.rotation = toQuat;
+                }
+                //waypointInd = WaypointManager.Instance.waypointInd - 1;
+                //waypointObj = GameObject.Find(String.Format("Waypoint{0}", waypointInd));
+                //coordTextObj = GameObject.Find(String.Format("WaypointCoord{0}", waypointInd));
+                Debug.Assert(coordTextObj != null);
+                coordText = coordTextObj.GetComponent<Text>();
+                string msg = string.Format("{0}\n({1}, {2})", waypointObj.name, Math.Round(waypointObj.transform.position.x, 1), Math.Round(waypointObj.transform.position.z, 1));
+                coordText.text = msg;
+                if (!waypointPlaced)
+                {
+                    waypointPlaced = true;
+                }
             }
-            GameObject coord_text_obj = GameObject.Find(String.Format("WaypointCoord{0}", waypoint_ind));
-            Text coord_text = coord_text_obj.GetComponent<Text>();
-            string msg = string.Format("{0}\n({1}, {2})", gameObject.name, Math.Round(gameObject.transform.position.x, 1), Math.Round(gameObject.transform.position.z, 1));
-            coord_text.text = msg;
         }
     }
 }
---
+++ Assets/Scripts/WaypointManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Academy.HoloToolkit.Unity
+{
+    public class WaypointManager : Singleton<WaypointManager>
+    {
+        public int waypointInd;
+        // Use this for initialization
+
+        void Start()
+        {
+            waypointInd = 0;
+        }
+
+        public void IncrementInd()
+        {
+            waypointInd++;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
